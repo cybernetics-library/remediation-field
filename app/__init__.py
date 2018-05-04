9ef237babd1216c908483fb6d4328220be7d9625
hxrts <sam@hxrts.com>
2018-05-04 14:13:40 -0400

    library thing API request

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from tinydb import TinyDB, where, Query
 import json
 import random
+import requests
 from flask import Flask, request, jsonify, redirect, render_template
 from flask_cors import CORS
 from collections import defaultdict
@@ -15,6 +16,26 @@ plots_db = db.table('plots')
 links_db = db.table('links')
 
 Group = Query()
+
+
+# initial Library thing dictionary fetch
+lib_thing = requests.get("http://www.librarything.com/api_getdata.php?userid=cyberneticscon&showstructure=1&max=1000&showTags=10&booksort=title_REV&responseType=json").json()['books']
+
+# fetch from Library Thing, replace global variable & return current dictionary
+@app.route('/lib_thing/fetch')
+def fetch_lib_thing():
+    global lib_thing
+    lib_thing = requests.get("http://www.librarything.com/api_getdata.php?userid=cyberneticscon&showstructure=1&max=1000&showTags=10&booksort=title_REV&responseType=json").json()['books']
+    return jsonify(lib_thing)
+
+# return existing Libary Thing dictionary without fetch
+@app.route('/lib_thing/current')
+def current_lib_thing():
+    global lib_thing
+    if lib_thing is None:
+        lib_thing = []
+    return jsonify(lib_thing)
+
 
 @app.route('/plot/link', methods=['POST'])
 def plot_link():
