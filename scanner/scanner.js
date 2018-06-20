30496ca366338d27e0b57e972793f6593671926a
melaniehoff <m@melanie-hoff.com>
2018-06-20 17:18:07 -0400

    full poem frontend system of api calls and most ui elements done

--- scanner/scanner.js
+++ scanner/scanner.js
@@ -1,12 +1,9 @@
-$(document).ready(function() {
+
 
   window.thisbook = "";
-
-  var sounds_dir = "wavs/";
-  var sounds_meow = ["meow1.wav", "meow2.wav", "meow3.wav", "meow4.wav", "meow5.mp3", "meow6.wav", "meow7.mp3", "meow8.wav", "meow9.wav", "meow10.wav",
-    "meow11.wav", "meow12.wav", "meow13.wav", "meow14.wav", "meow15.mp3", "meow16.mp3"
-  ];
-  var sounds_bell = ["idea.wav"];
+  var globalQR;
+  var memArray;
+  var theme;
 
   window.prevlink = {};
 
@@ -45,6 +42,8 @@ $(document).ready(function() {
   window.urlToId = urlToId;
 
   function parseQR(content) {
+
+
     var res = {};
     res.books = {};
     res.names = {};
@@ -67,59 +66,70 @@ $(document).ready(function() {
     return res;
   }
 
+  function sameBookTimer() {
+      window.prevlink = {};
+  };
+
   function handleScans(content) {
-    /* see what's in the scans, how many we have
-     and decide what to do */
 
-    if(content.length == 2) {
-      if(isBook(content[0]) && isName(content[1])) {  makeLink(content); }
-      if(isBook(content[1]) && isName(content[0])) { makeLink(content);  }
-    }
-  }
+    setTimeout(sameBookTimer, 12000);
+$("iframe").fadeIn(300);
 
-  function makeLink(content) {
     var res = parseQR(content);
     if(!(_.isEqual(window.prevlink, res))) {
-      window.prevlink = res;
-      console.log(res);
-      console.log("MADEL INK!!!");
-      submitLinkToApi(res);
-    }
-  }
+        window.prevlink = res;
+        console.log(res);
+        console.log("new book");
+        globalQR = content;
+           displayModal();
+      };
+  };
 
-  function submitLinkToApi(res) {
-    //plot/link [POST]: Link a plot to a book. Needs book_id, plot_id, station_id, timestamp.
+  function submitLinkToApi(content) {
+    var res = parseQR(content);
     window.restest = res;
+    // var mem = $( "#mem-id" ).html();
+    // var theme = document.getElementById("theme-id").value;
+    // var memTo = document.getElementById("mem-to-id").value;
     var bookid = urlToId(res.books[Object.keys(res.books)[0]]);
-    var plotid = urlToId(res.names[Object.keys(res.names)[0]]);
-    var apiurl = "https://library.cybernetics.social/plot/link";
-    var data = {
-      'book_id':  bookid,
-      'plot_id':  plotid,
-      'station_id': "webscanner",
-      'timestamp': new Date().getTime() / 1000
-    };
-    console.log(data);
+    // var apiurl = "https://library.cybernetics.social/connect_book_to_memory";
+
+    $.post("https://library.cybernetics.social/connect_book_to_memory",
+    {
+    "book_id": bookid,
+    "station_id": "computer1",
+    "timestamp": new Date().getTime() / 1000,
+    "memory_from": $( "#mem-id" ).html(),
+    "memory_to": document.getElementById("mem-to-id").value,
+    "theme": theme
+  },
+    function (response) {
+        console.dir(response);
+      });
+  };
+
+
+  function displayModal() {
+    $("#prompt-1").fadeOut(300).delay(2000);
+    $("video").addClass( "blur grayscale" );
+    $("#prompt-2").fadeIn(400).delay(2000);
+    $("#mem-to-id").val('');
+    // TODO:
+
+    var apiurl = "https://library.cybernetics.social/memories/unique";
     $.ajax({
-      type: "POST",
+      type: "GET",
       url: apiurl,
       contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
       dataType: "json",
       success: function(res) {
-        console.log(res);
-        console.log("successfully linked!")
-        displayModal()
+        console.log(res.memories_all);
+        console.log("successfully submitted!")
+        memArray = res.memories_all;
       }
     });
 
 
-
-  }
-
-
-  function displayModal() {
-    $("#link_success").fadeIn(400).delay(2000).fadeOut(300);
   }
 
   function updateIframe(content,res) {
@@ -132,6 +142,7 @@ $(document).ready(function() {
     ///////////////////////////////////////
     var res = parseQR(content);
     var thisbookids = Object.keys(res.books).sort()
+    console.log("This book id: " + thisbookids);
 
     var thisbooks = thisbookids.join("+");
     if ((thisbooks) && (window.prevbooks != thisbooks)) {
@@ -144,16 +155,11 @@ $(document).ready(function() {
       // console.log(s);
       $("#iframe").attr("src", "https://www.librarycat.org/lib/CyberneticsCon/item/" + thisbookids[0]);
       window.prevbooks = thisbooks;
-      sound = sounds_meow[hashCode(thisbookids[0]) % sounds_meow.length]
-      //          var sound = _.sample(sounds_meow);
-      var sound = _.sample(sounds_meow);
-      var snd = new Audio(sounds_dir + sound); // buffers automatically when created
-      snd.play();
     }
   }
 
 
-
+$(document).ready(function() {
 
 
   let scanner = new Instascan.Scanner({
@@ -180,4 +186,67 @@ $(document).ready(function() {
   });
 
 
+// TODO:
+//random theme
+
+
+randomTheme();
+
+$.ajax({
+  type: "GET",
+  url: "https://library.cybernetics.social/memories/unique",
+  contentType: "application/json; charset=utf-8",
+  dataType: "json",
+  success: function(res) {
+    console.log(res.memories_all);
+    console.log("successfully requested mem_from!")
+    memArray = res.memories_all;
+    var randomMem = memArray[Math.floor(Math.random()*memArray.length)];
+    $('#mem-id').html(randomMem);
+  }
 });
+
+var input = document.getElementById("mem-to-id");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    sendMem();
+  }
+});
+
+
+});
+
+function randomTheme(){
+themeArray = ["heart", "vision", "mouth", "body"];
+var randomTheme = themeArray[Math.floor(Math.random()*themeArray.length)];
+console.log(randomTheme);
+theme = randomTheme
+$('.theme').html(theme);
+};
+
+function sendMem() {
+ submitLinkToApi(globalQR);
+ randomTheme();
+ $("#success").fadeIn(300).delay(2000);
+  $("#success").fadeOut(300).delay(3400);
+
+
+  setTimeout(showPrompt, 3400);
+ $("#prompt-2").fadeOut(300).delay(2000);
+ $("iframe").fadeOut(300);
+
+};
+
+function showPrompt(){
+  $('#mem-id').clear;
+  var randomMem = memArray[Math.floor(Math.random()*memArray.length)];
+  $('#mem-id').html(randomMem);
+  $("video").removeClass( "blur grayscale" );
+  $("#prompt-1").fadeIn(300);
+}
