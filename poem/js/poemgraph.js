e9053c8510f8ea5bd9cd57db920685af125e6404
Dan Taeyoung <provolot@gmail.com>
2018-06-20 17:55:32 -0400

    use bookdata

--- poem/js/poemgraph.js
+++ poem/js/poemgraph.js
@@ -1,12 +1,13 @@
 var poemgraph = {};
 
-poemgraph.makeGraph = function(rawdata) {
+
+poemgraph.graphDataFromRawData = function(rawdata, bookdata) {
 
   var graph = {};
 
   //formatteddata = _.mapValues(_.groupBy(rawdata, 'memory_from'), function(d) { return _.map(d, 'memory_to'); })
 
-  graph.links = _.concat(
+  var rawlinks = _.concat(
       _.map(vueapp.rawdata, function(d) {
                  return { 'source': d.memory_from, 'target': d.book_id };
             }),
@@ -14,11 +15,30 @@ poemgraph.makeGraph = function(rawdata) {
                    return { 'source': d.book_id, 'target': d.memory_to };
               })
   );
+  graph.links = _.map(
+    _.groupBy(rawlinks, function(d) {return d.source + d.target}),
+    function(d) { 
+        return _.assign(d[0], { 'value': d.length});
+    })
+
+
   graph.nodes = _.map(_.uniq( _.concat(_.map(vueapp.rawdata, 'book_id'), _.map(vueapp.rawdata, 'memory_to'), _.map(vueapp.rawdata, 'memory_from'))), function(d) {
-      return { 'id': d };
+      return { 'id': d, 'name': d };
   });
 
-  var svg = d3.select("svg"),
+  return graph;
+}
+
+
+
+
+
+poemgraph.makeGraph = function(rawdata, bookdata) {
+
+  graph = poemgraph.graphDataFromRawData(rawdata, bookdata);
+
+  d3.selectAll("#d3svg g").remove();
+  var svg = d3.select("#d3svg"),
         width = +svg.attr("width"),
         height = +svg.attr("height");
 
@@ -41,16 +61,24 @@ poemgraph.makeGraph = function(rawdata) {
           .attr("class", "nodes")
           .selectAll("circle")
           .data(graph.nodes)
-          .enter().append("circle")
-          .attr("r", 5)
-          .attr("fill", function(d) { return color(d.group); })
+          .enter().append("g")
+          .attr("class","node")
           .call(d3.drag()
                       .on("start", dragstarted)
                       .on("drag", dragged)
                       .on("end", dragended));
+
+      node.append('circle')
+          .attr("r", 50)
+//          .attr("fill", function(d) { return color(d.group); })
     
-      node.append("title")
-          .text(function(d) { return d.id; });
+      node.append("text")
+        .attr("dx", 12)
+        .attr("dy", ".35em")
+
+          .text(function(d) { return d.name; });
+
+
     
       simulation
           .nodes(graph.nodes)
@@ -66,9 +94,13 @@ poemgraph.makeGraph = function(rawdata) {
                 .attr("x2", function(d) { return d.target.x; })
                 .attr("y2", function(d) { return d.target.y; });
         
-            node
-                .attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
+            //node
+                //.attr("cx", function(d) { return d.x; })
+                //.attr("cy", function(d) { return d.y; });
+
+            node.attr("transform", function(d, i) {     
+                    return "translate(" + d.x + "," + d.y + ")"; 
+                });
           }
 
   function dragstarted(d) {
