00a22054e17dff8e427ab2fb147bf1217a79414b
Dan Taeyoung <provolot@gmail.com>
2018-06-20 16:23:27 -0400

    d3 working

---
+++ poem/js/poemgraph.js
@@ -0,0 +1,91 @@
+var poemgraph = {};
+
+poemgraph.makeGraph = function(rawdata) {
+
+  var graph = {};
+
+  //formatteddata = _.mapValues(_.groupBy(rawdata, 'memory_from'), function(d) { return _.map(d, 'memory_to'); })
+
+  graph.links = _.concat(
+      _.map(vueapp.rawdata, function(d) {
+                 return { 'source': d.memory_from, 'target': d.book_id };
+            }),
+        _.map(vueapp.rawdata, function(d) {
+                   return { 'source': d.book_id, 'target': d.memory_to };
+              })
+  );
+  graph.nodes = _.map(_.uniq( _.concat(_.map(vueapp.rawdata, 'book_id'), _.map(vueapp.rawdata, 'memory_to'), _.map(vueapp.rawdata, 'memory_from'))), function(d) {
+      return { 'id': d };
+  });
+
+  var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+  var simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().id(function(d) { return d.id; }))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2));
+
+
+      var link = svg.append("g")
+          .attr("class", "links")
+          .selectAll("line")
+          .data(graph.links)
+          .enter().append("line")
+          .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+    
+      var node = svg.append("g")
+          .attr("class", "nodes")
+          .selectAll("circle")
+          .data(graph.nodes)
+          .enter().append("circle")
+          .attr("r", 5)
+          .attr("fill", function(d) { return color(d.group); })
+          .call(d3.drag()
+                      .on("start", dragstarted)
+                      .on("drag", dragged)
+                      .on("end", dragended));
+    
+      node.append("title")
+          .text(function(d) { return d.id; });
+    
+      simulation
+          .nodes(graph.nodes)
+          .on("tick", ticked);
+    
+      simulation.force("link")
+          .links(graph.links);
+    
+      function ticked() {
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+        
+            node
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+          }
+
+  function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+  }
+
+  function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+  }
+
+}
