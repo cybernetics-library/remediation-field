bd94ff919b2dfb9aa900815c5bef801c3e09cc45
Cybernetics Library <dan@dantaeyoung.com>
2018-06-20 02:14:29 -0400

    gunicorn working with socketio

--- start_library_api.sh
+++ start_library_api.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source venv/bin/activate
-gunicorn --bind 0.0.0.0:5000 wsgi
+gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:5000 wsgi
+#gunicorn --bind 0.0.0.0:5000 wsgi
 deactivate
